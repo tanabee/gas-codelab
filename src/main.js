2044fa5d275c2e065ca94bf312bcdfcf352035c3
tanabee <tanabee1986@gmail.com>
2019-11-04 21:08:22 +0900

    Add source code

---
+++ src/main.js
@@ -0,0 +1,41 @@
+function onOpen() {
+  SpreadsheetApp
+    .getActiveSpreadsheet()
+    .addMenu('Gmail', [
+      {name: 'Fetch', functionName: 'main'},
+      {name: 'Clear sheet', functionName: 'clearSheet'},
+    ]);
+}
+
+function main() {
+  var threads = GmailApp.getInboxThreads(0, 200);
+  var messages = [['Subject', 'From', 'To', 'Body', 'Date']];
+  threads.forEach(function (thread) {
+    var message = thread.getMessages()[0];
+    if (message.getPlainBody().length > 10000) {
+      return;
+    }
+    messages.push([
+      message.getSubject(),
+      message.getFrom(),
+      message.getTo(),
+      message.getPlainBody(),
+      message.getDate(),
+    ]);
+  });
+  insertSpreadSheet(messages);
+}
+
+function insertSpreadSheet(data) {
+	clearSheet();
+  SpreadsheetApp
+    .getActiveSheet()
+    .getRange("A1:E" + data.length)
+    .setValues(data);
+}
+
+function clearSheet() {
+  SpreadsheetApp
+    .getActiveSheet()
+    .clear();
+}
